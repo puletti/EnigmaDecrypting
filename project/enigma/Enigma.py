3f321d66b184f3e2d5766488c48e735dadade2fb
puletti <114535245+puletti@users.noreply.github.com>
2024-02-04 16:27:23 +0100

    Update Enigma.py

--- project/enigma/Enigma.py
+++ project/enigma/Enigma.py
@@ -1,6 +1,7 @@
-from Rotor import Rotor
-from Reflector import Reflector
-from Plugboard import Plugboard
+from enigma.Rotor import Rotor
+from enigma.Reflector import Reflector
+from enigma.Plugboard import Plugboard
+
 class Enigma:
     # 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
     # A B C D E F G H I J K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
